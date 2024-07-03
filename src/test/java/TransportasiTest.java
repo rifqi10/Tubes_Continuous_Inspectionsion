38238ab7b52d409b25ebc669c8e27432d9a8e63c
rifqi10 <53550730+rifqi10@users.noreply.github.com>
2024-07-03 20:59:20 +0700

    Revert "Merge pull request #3 from rifqi10/find"
    This reverts commit ba161cf2280584ea1e9e2317cf0e040dac61b2c8, reversing
    changes made to 3d725aeaafa954b9a5c36962fb43036622961945.

--- src/test/java/TransportasiTest.java
+++ src/test/java/TransportasiTest.java
@@ -1,8 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
-import transportasi.*;  
-ppppppppppppppppppppppppppppppppppppppppp
+import transportasi.*;
 
 public class TransportasiTest {
     
